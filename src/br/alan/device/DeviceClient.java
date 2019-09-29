e0117554aafccbad457a08edd3e6dde74098717d
Alan Souza Fagundes <alan.souza@solutis.com.br>
2019-09-29 20:08:15 -0300

    Crypto

--- src/br/alan/device/DeviceClient.java
+++ src/br/alan/device/DeviceClient.java
@@ -1,10 +1,13 @@
 package br.alan.device;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
+import javax.crypto.*;
+import javax.xml.bind.DatatypeConverter;
+import java.io.*;
 import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.Scanner;
 
 public class DeviceClient {
@@ -31,31 +34,44 @@ public class DeviceClient {
         this.deviceSettings = deviceSettings;
     }
 
-    public String getName(){
+    public String getName() {
         return this.deviceSettings.getName();
     }
 
-    public  OutputStream getOutputStream() throws IOException {
+    public OutputStream getOutputStream() throws IOException {
         return this.socket.getOutputStream();
     }
 
-    public  InputStream getInputStream() throws IOException {
+    public InputStream getInputStream() throws IOException {
         return this.socket.getInputStream();
     }
 
 
-    public  PrintStream getPrintStream() throws IOException {
-//        if (this.printStream == null) {
+    public PrintStream getPrintStream() throws IOException {
+        if (this.printStream == null) {
             return new PrintStream(this.getOutputStream());
-//        }
-//        return this.printStream;
+        }
+        return this.printStream;
     }
 
-    public  Scanner getScanner() throws IOException {
-//        if (this.scanner == null) {
+    public Scanner getScanner() throws IOException {
+        if (this.scanner == null) {
             return new Scanner(this.getInputStream());
-//        }
-//        return this.scanner;
+        }
+        return this.scanner;
+    }
+
+    public synchronized void sendEncryptedMessage(String message, String key, String algorithm)
+            throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, BadPaddingException,
+            IllegalBlockSizeException, InvalidKeyException {
+
+        String messageEncryptedString = this.encrypt(message, key, algorithm);
+        this.send(messageEncryptedString);
+    }
+
+    public String getNextEcryptedMessage(String key, String algorithm) throws IOException, NoSuchAlgorithmException,
+            NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException {
+        return this.decrypt(this.nextMessage(), key, algorithm);
     }
 
     public synchronized void send(String message) throws IOException {
@@ -64,7 +80,7 @@ public class DeviceClient {
         printStream.flush();
     }
 
-    public  void printAllMessages() throws IOException {
+    public void printAllMessages() throws IOException {
         Scanner sc = this.getScanner();
 
         while (sc.hasNextLine()) {
@@ -74,7 +90,7 @@ public class DeviceClient {
         this.device.print(" Ending conection with " + deviceSettings.getName());
     }
 
-    public  String nextMessage() throws IOException {
+    public String nextMessage() throws IOException {
         Scanner sc = this.getScanner();
 
         if (sc.hasNextLine()) {
@@ -84,15 +100,51 @@ public class DeviceClient {
         return null;
     }
 
+
     public Socket getSocket() {
         return socket;
     }
 
-    public void setSocket(Socket socket) {
-        this.socket = socket;
-    }
-
-    public  void die() throws IOException {
+    public void die() throws IOException {
         this.socket.close();
     }
+
+
+    public static String encrypt(String toEncrypt, String key, String algorithm) throws NoSuchAlgorithmException,
+            NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, UnsupportedEncodingException {
+        // create a binary key from the argument key (seed)
+        SecureRandom sr = new SecureRandom(key.getBytes());
+        KeyGenerator kg = KeyGenerator.getInstance(algorithm);
+        kg.init(sr);
+        SecretKey sk = kg.generateKey();
+
+        // create an instance of cipher
+        Cipher cipher = Cipher.getInstance(algorithm);
+
+        // initialize the cipher with the key
+        cipher.init(Cipher.ENCRYPT_MODE, sk);
+
+        // enctypt!
+        byte[] encrypted = cipher.doFinal(toEncrypt.getBytes());
+
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    public static String decrypt(String toDecrypt, String key, String algorithm) throws NoSuchAlgorithmException,
+            NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, UnsupportedEncodingException {
+        // create a binary key from the argument key (seed)
+        SecureRandom sr = new SecureRandom(key.getBytes());
+        KeyGenerator kg = KeyGenerator.getInstance(algorithm);
+        kg.init(sr);
+        SecretKey sk = kg.generateKey();
+
+        // do the decryption with that key
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.DECRYPT_MODE, sk);
+        byte[] decrypted = cipher.doFinal(Base64.getDecoder().decode(toDecrypt));
+
+        return new String(decrypted);
+    }
 }
+
+
